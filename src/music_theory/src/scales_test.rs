456825b1f38b9da5787e6f1a17bf553f17536a01
Swan BENOIT <swan.benoit@gmail.com>
2022-05-05 17:28:54 +0200

    Add scale test

--- src/music_theory/src/scales_test.rs
+++ src/music_theory/src/scales_test.rs
@@ -22,5 +22,36 @@ mod scales_test {
             Note::new(F, Sharp, 1),
             Note::new(G, Natural, 1)
         ]));
+
+        let scale = Scale::new(
+            ScaleType::Major,
+            Note::new(F, Sharp, 0),
+        );
+        assert_eq!(scale.get_notes(), Vec::from([
+            Note::new(F, Sharp, 0),
+            Note::new(G, Sharp, 0),
+            Note::new(A, Sharp, 0),
+            Note::new(B, Natural, 0),
+            Note::new(C, Sharp, 1),
+            Note::new(D, Sharp, 1),
+            Note::new(E, Sharp, 1),
+            Note::new(F, Sharp, 1),
+        ]));
+
+
+        let scale = Scale::new(
+            ScaleType::Major,
+            Note::new(D, Flat, 0),
+        );
+        assert_eq!(scale.get_notes(), Vec::from([
+            Note::new(D, Flat, 0),
+            Note::new(E, Flat, 0),
+            Note::new(F, Natural, 0),
+            Note::new(G, Flat, 0),
+            Note::new(A, Flat, 0),
+            Note::new(B, Flat, 0),
+            Note::new(C, Natural, 1),
+            Note::new(D, Flat, 1),
+        ]));
     }
 }
